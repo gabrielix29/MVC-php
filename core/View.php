e089f017379e9ff32b5b3ccec14f03556a4141eb
gabrielix29 <>
2020-07-31 16:06:16 +0200

    add View to core

---
+++ core/View.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MVC\core;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+class View
+{
+    private string $defaultTemplate;
+    private string $viewDir;
+    private array $data;
+
+    function __construct()
+    {
+        $this->defaultTemplate = getenv("DefaultTemplate");
+        $this->data = array();
+    }
+
+    public function render(string $view, array $data = null)
+    {
+        if ($data) {
+            $this->data = $data;
+        }
+
+        $loader = new FilesystemLoader(dirname(__DIR__) . '/views');
+        $twig = new Environment($loader);
+        echo $twig->render($view, $this->data);
+    }
+
+    public function setData(array $data) {
+        $this->data = $data;
+    }
+
+    public function addData(array $data) {
+        array_push($this->data, $data);
+    }
+
+    public function overrideDefaultTemplate(string $template)
+    {
+        $this->defaultTemplate = $template;
+    }
+}
