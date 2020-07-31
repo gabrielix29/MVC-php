a4e46a139421522ed0fdb58127fed6421ba1fcfc
gabrielix29 <>
2020-07-31 15:52:31 +0200

    use static DotEnv

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use MVC\utils\DotEnv;
 
-$dotEnv = new DotEnv();
-$dotEnv->load();
+DotEnv::load();
