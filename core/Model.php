8cdacfd38829b878b08b57d68b31d2b4ef9ae557
gabrielix29 <>
2020-07-31 16:16:14 +0200

    update Model

--- core/Model.php
+++ core/Model.php
@@ -2,7 +2,14 @@
 
 namespace MVC\classes;
 
+use MVC\utils\DatabaseConnector;
+
 abstract class Model
 {
+    private DatabaseConnector $dbConn;
 
+    function __construct()
+    {
+        $this->dbConn = new DatabaseConnector();
+    }
 }
