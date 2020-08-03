b62f14a2db0d8839106c5a636e1a0be2ae174cff
gabrielix29 <gabriel@guillet.ch>
2020-08-03 10:39:27 +0200

    use right form of callable for Router

--- public/index.php
+++ public/index.php
@@ -5,9 +5,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use MVC\core\Router;
 use MVC\utils\DotEnv;
 
+use MVC\app\controllers\MainController;
+
 DotEnv::load();
 
 $router = new Router();
 
-$router->add("GET", "/\//", ["MVC\\app\\controllers\\MainController", "index"]);
+$router->add("GET", "/\//", [new MainController(), "index"]);
 $router->run();
